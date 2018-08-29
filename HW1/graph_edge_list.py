c588de218bb272deee00fbbf9ed03dc056566626
marikacusick <mmc265@cornell.edu>
2018-08-29 10:56:26 -0400

    added HW1 files

---
+++ HW1/graph_edge_list.py
@@ -0,0 +1,40 @@
+# Please see instructions.txt for the description of this problem.
+from exceptions import NotImplementedError
+
+# An implementation of a weighted, directed graph as an edge list. This means
+# that it's represented as a list of tuples, with each tuple representing an
+# edge in the graph.
+class Graph:
+  def __init__(self):
+    # DO NOT EDIT THIS CONSTRUCTOR
+    self.graph = []
+
+  def add_edge(self, node1, node2, weight):
+    # Adds a directed edge from `node1` to `node2` to the graph with weight
+    # defined by `weight`.
+    
+    edge = tuple((node1,node2, weight))
+    print (edge)
+    self.graph.append(edge)
+  
+  def has_edge(self, node1, node2):
+    # Returns whether the graph contains an edge from `node1` to `node2`.
+    # DO NOT EDIT THIS METHOD
+    return (node1, node2) in [(x,y) for (x,y,z) in self.graph]
+
+  def get_neighbors(self, node):
+    # Returns the neighbors of `node` as a list of tuples [(x, y), ...] where
+    # `x` is the neighbor node, and `y` is the weight of the edge from `node`
+    # to `x`.
+    
+    neigh_list = []
+    for edge in self.graph:
+        if node in edge:
+            for i in range(2):
+                if edge[i] != node:
+                    neigh = edge[i]
+            weight = edge[2]
+            temp_tuple = tuple((neigh, weight))
+            neigh_list.append(temp_tuple)
+
+    return neigh_list
