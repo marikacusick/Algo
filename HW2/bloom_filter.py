e94f0eb8c3a84300b348286b4d9e5a244a58861f
Travis Allen <travis.william.allen@gmail.com>
2018-09-11 15:20:32 -0400

    add_elem and check_membership method working, and bloom_filter_test passes

--- HW2/bloom_filter.py
+++ HW2/bloom_filter.py
@@ -14,11 +14,29 @@ class BloomFilter:
       self.array.set(i, False)
 
   # Adds an element to the bloom filter using three hash functions.
-  def add_elem(self, elem):
+  def add_elem(self, elem):    
+    index_to_add_h1 = cs5112_hash1(elem) % self.size
+    self.array.set(index_to_add_h1, True)
+        
+    index_to_add_h2 = cs5112_hash2(elem) % self.size
+    self.array.set(index_to_add_h2, True)
+       
+    index_to_add_h3 = cs5112_hash3(elem) % self.size
+    self.array.set(index_to_add_h3, True)
+    
+    return
+    
     raise NotImplementedError
+    
 
   # Returns False if the given element is was definitely not added to the
   # filter. Returns True if it's possible that the element was added to the
   # filter (but not necessarily certain).
   def check_membership(self, elem):
+    index_h1 = cs5112_hash1(elem) % self.size
+    index_h2 = cs5112_hash2(elem) % self.size
+    index_h3 = cs5112_hash3(elem) % self.size
+    
+    return(self.array.get(index_h1) and self.array.get(index_h2) and self.array.get(index_h3))
+
     raise NotImplementedError
